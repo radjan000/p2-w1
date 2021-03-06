37b5d692799fca87e60eccc3f86eade9579ca3e4
radjan000 <radjan000@utp.edu.pl>
2021-03-06 09:03:21 +0100

    p2 w01 v020

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+// psvm -> public static void main(String[] args){}
+// sout -> System.out.println();
+// CTRL + D -> duplikacja linijki
+// ALT + Ins -> generator kodu
+// CTRL + ALT + L -> formatowanie
+// CTRL + / koment/odkoment
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO!");
+        User u1 = new User(email:"rj@rj.pl", password: "rj");
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,10 @@
+// ALT + Ins - generate
+public class User {
+    private String email;
+    private String password;
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
