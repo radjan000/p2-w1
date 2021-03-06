cb64d76930e3da325d106b8f84d5ecd8191751ca
radjan000 <radjan000@utp.edu.pl>
2021-03-06 09:18:32 +0100

    p2_w01_v03

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HELLO!");
         User u1 = new User(email:"rj@rj.pl", password: "rj");
+        User u2 = new User(email:"rj2@rj.pl", password: "rj2");
+        User u3 = new User(email:"rj3@rj.pl", password: "rj3");
+        System.out.println(u1.toString());
+        System.out.println(u2.toString());
+        System.out.println("STOP");
+
     }
 }
